a69088ff1553d0d7afe4df04974cbf7a9127ed3b
Yimeng Yu <yimeng.yu@gmail.com>
2020-01-09 19:13:10 -0800

    ake the project frame

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+// Set up Mysql connection.
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db"
+});
+
+//Make connection.beginTransaction
+
+connection.connect(function(err){
+    if(err) {
+        console.log("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
